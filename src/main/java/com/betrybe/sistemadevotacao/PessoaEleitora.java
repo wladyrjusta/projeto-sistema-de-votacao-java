0429000a8a08f260da75057ee8db1c85d30a277a
wladyrjusta <wladyr.justa@gmail.com>
2023-09-22 16:16:09 -0300

    Implementada classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,46 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    this.cpf = cpf;
+    this.nome = nome;
+  }
+
+  /**
+   * Obtém o nome da candidata.
+   *
+   * @return O nome da candidata.
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  /**
+   * Obtém o cpf da candidata.
+   *
+   * @return O cpf da candidata.
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Define o nome da candidata.
+   *
+   * @param nome O nome da candidata.
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * Define o cpf da candidata.
+   *
+   * @param cpf O nome da candidata.
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
